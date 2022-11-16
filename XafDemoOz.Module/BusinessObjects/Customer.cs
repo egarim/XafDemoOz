6834fe8c1c4dafec5440ea806915e6c5dc2351f5
Jose Ojeda <joche.ojeda@bitframeworks.com>
2022-11-16 12:41:30 +0300

    Add project files.

---
+++ XafDemoOz.Module/BusinessObjects/Customer.cs
@@ -0,0 +1,73 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafDemoOz.Module.BusinessObjects
+{
+    [Appearance("Disable adddress",Criteria ="Active = false",TargetItems ="Address",Enabled =false)]
+    [DefaultClassOptions]
+    public class Customer : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Customer(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        bool active;
+        Category category;
+        string address;
+        ///as , ps
+        //xps
+
+        string name;
+        [RuleRequiredField()]
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+        [Size(SizeAttribute.Unlimited)]
+        public string Address
+        {
+            get => address;
+            set => SetPropertyValue(nameof(Address), ref address, value);
+        }
+
+        //xpo
+        //[VisibleInDetailView(false)]
+        //[VisibleInListView(false)]
+
+        //[Browsable(false)]
+        public Category Category
+        {
+            get => category;
+            set => SetPropertyValue(nameof(Category), ref category, value);
+        }
+        [ImmediatePostData()]
+        public bool Active
+        {
+            get => active;
+            set => SetPropertyValue(nameof(Active), ref active, value);
+        }
+
+    }
+}
